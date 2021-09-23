54055569b6390bd0f55c95c1b0ea7c3351ccc636
hassamSatti <hassamsatti1996@gmail.com>
2021-09-23 12:55:17 +0500

    Main

---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'User List',
+            'User Create',
+            'User Edit',
+            'User Delete',
+            'Role List',
+            'Role Create',
+            'Role Edit',
+            'Role Delete',
+            'Permission List',
+            'Permission Create',
+            'Permission Edit',
+            'Permission Delete',
+            'Post List',
+            'Post Create',
+            'Post Edit',
+            'Post Delete',
+
+        ];
+
+        foreach ($data as $permission) {
+             Permission::create(['name' => $permission]);
+        }
+    }
+}
